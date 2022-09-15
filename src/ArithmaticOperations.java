93a85441f8303387f4ec85d7491e2bddd452a6f4
yonghoow <2073951b@student.tp.edu.sg>
2022-09-15 10:34:41 +0800

    commit on Sep 15

---
+++ src/ArithmaticOperations.java
@@ -0,0 +1,10 @@
+package src;
+
+public class ArithmaticOperations {
+    public static void main(String[] args) {
+        System.out.println(4 + 5);
+        System.out.println(4 * 5);
+        System.out.println(4 / 5);
+        System.out.println(9 % 2);
+    }
+}
---
+++ src/DIsplayNonASCIICharacters.java
@@ -0,0 +1,9 @@
+package src;
+
+public class DIsplayNonASCIICharacters {
+    public static void main(String[] args) {
+        System.out.println("Non ASCII characters");
+        System.out.println("日本人 中國的 ~=[]()%+{}@;’#!$_&-  éè  ;∞¥₤€");
+        System.out.println("pi = " + 3.14); //+ is used to concat //
+    }
+}
